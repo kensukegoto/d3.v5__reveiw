47314a6cc27365c152b0b6aebf49f9cb3104e9ac
kensuke goto <kensuke.goto.v5@gmail.com>
2020-02-23 02:50:48 +0900

    1000ミリ秒ごとにグラフが切り替わる

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@
 *    Project 1 - Star Break Coffee
 */
 
-
 const m = {
   t: 20,
   l: 100,
@@ -25,14 +24,14 @@ let svg = d3.select("#chart-area")
 let g = svg.append("g")
   .attr("transform","translate("+ m.l +","+ m.t +")");
 
-g.append("text")
+let xLabel = g.append("text")
   .attr("x",width / 2)
   .attr("y",height + 50)
   .attr("text-anchor","middle")
   .attr("font-size","20px")
   .text("Month")
 
-g.append("text")
+let yLabel = g.append("text")
   .attr("x",-(height / 2))
   .attr("y",-60)
   .attr("transform","rotate(-90)")
@@ -45,44 +44,78 @@ d3.json("./data/revenues.json").then(data =>{
   
   data.forEach( e => {
     e.revenue = + e.revenue;
-    e.revenue = + e.revenue;
+    e.profit = + e.profit;
   });
-
   let x = d3.scaleBand()
-    .domain(data.map(e => e.month))
     .range([0,width])
     .paddingInner(.3)
     .paddingOuter(.3)
 
   let y = d3.scaleLinear()
-    .domain([0,d3.max(data,d=>d.revenue)])
     .range([height,0])
 
-
-  let xAxisCall = d3.axisBottom(x);
-  g.append("g")
+  let xAxis = g.append("g")
     .attr("class","top axis")
     .attr("transform","translate(0,"+ height +")")
-    .call(xAxisCall)
+
+  let yAxis = g.append("g")
+    .attr("class","top axis")
+    
+
+  let flag = true;
+
+  d3.interval(()=>{
+    update();
+    flag = !flag;
+  },1000)
+
+  function update(){
+
+    const which = flag ? "revenue" : "profit";
+
+    x.domain(data.map(e => e.month))
+    y.domain([0,d3.max(data,d=>d[which])])
+
+  xAxis
+    .call(d3.axisBottom(x))
       .selectAll("text")
       .attr("text-anchor","middle")
-
-  let yAxisCall = d3.axisLeft(y)
-    .tickFormat(d => "$" + d)
-    
-  g.append("g")
-    .attr("class","top axis")
-    .call(yAxisCall)
+ 
+  yAxis
+    .call(
+      d3.axisLeft(y)
+      .tickFormat(d => "$" + d)
+    )
 
   let rects = g.selectAll("rect")
     .data(data)
 
+  rects.exit().remove()
+
+  rects
+    .attr("x",d => x(d.month))
+    .attr("width",x.bandwidth)
+    .attr("y",d => y(d[which]))
+    .attr("height",d => (height - y(d[which])))
+    .attr("fill","grey")
+
   rects.enter().append("rect")
     .attr("x",d => x(d.month))
     .attr("width",x.bandwidth)
-    .attr("y",d => y(d.revenue))
-    .attr("height",d => (height - y(d.revenue)))
+    .attr("y",d => y(d[which]))
+    .attr("height",d => (height - y(d[which])))
     .attr("fill","grey")
+
+  // rects.update().append("rect")
+  //   .attr("x",d => x(d.month))
+  //   .attr("width",x.bandwidth)
+  //   .attr("y",d => y(d[which]))
+  //   .attr("height",d => (height - y(d[which])))
+  //   .attr("fill","grey")
+    
+  yLabel.text(which.charAt(0).toUpperCase() + which.slice(1))
+
+  }
 
 
 
