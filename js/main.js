bb37cddbfbdc0160a3cb02a218f7da2cab79a460
kensuke goto <kensuke.goto.v5@gmail.com>
2020-02-23 16:23:16 +0900

    (fix)円の面積を求める関数

--- js/main.js
+++ js/main.js
@@ -29,19 +29,25 @@ d3.json("data/data.json").then(function(data){
 	
 	// データ整形
 	data = data.map(function(year){
-
-		const countries = year["countries"].filter(function(country){
+		
+		const countries = year.countries.filter(function(country){
 				var dataExists = (country.income && country.life_exp);
 				return dataExists
 		}).map(function(country){
 				country.income = +country.income;
 				country.life_exp = +country.life_exp;
-				return country;            
+				return country;       
 		})
+
+		const range = d3.extent(countries,d => d.population);
 		return {
-			year: +year.year,countries
+			countries,
+			min:range[0],
+			max:range[1],
+			year: +year.year,
 		}
 	});
+
 
 	let x = d3.scaleLog()
 		.domain([300,150000])
@@ -50,7 +56,12 @@ d3.json("data/data.json").then(function(data){
 		.domain([0,90])
 		.range([h2,0])
 	let r = d3.scaleLinear()
-		.range([5,25])
+		.domain([
+			d3.min(data,d => d.min),
+			d3.max(data,d => d.max)
+		])
+		.range([Math.pow(5,2) * Math.PI, Math.pow(50,2) * Math.PI]) // 直径： 最小 10px,最大100px
+
 
 	let c = d3.scaleOrdinal(d3.schemePastel1)
 		// .domain(["africa","americas","asia","europe"])
@@ -74,11 +85,11 @@ d3.json("data/data.json").then(function(data){
 		const t = d3.transition()
 			.duration(100);
 
-		r
-		.domain([
-			d3.min(data,d => d.population) / 5 * Math.PI,
-			d3.max(data,d => d.population) / 5 * Math.PI
-		])
+		// r
+		// .domain([
+		// 	d3.min(data,d => d.population) / 5 * Math.PI,
+		// 	d3.max(data,d => d.population) / 5 * Math.PI
+		// ])
 
 		let scat = g.selectAll("circle")
 			.data(data,d => d.country)
@@ -90,7 +101,7 @@ d3.json("data/data.json").then(function(data){
 			.transition(t)
 			.attr("cx",d => x(d.income))
 			.attr("cy",d => y(d.life_exp))
-			.attr("r",d => r(d.population))
+			.attr("r",d => Math.sqrt(r(d.population)/Math.PI))
 			.attr("fill",d => c(d.continent))
 	}
 
@@ -108,10 +119,4 @@ d3.json("data/data.json").then(function(data){
 
 	update(data[idx % len].countries);
 
-
-
-
-
-	
-	
 })
