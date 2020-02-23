67419b96af116f112c6484ede92c24b9c1111bd5
kensuke goto <kensuke.goto.v5@gmail.com>
2020-02-23 14:29:52 +0900

    静的に更新される

--- js/main.js
+++ js/main.js
@@ -1,137 +1,95 @@
 /*
 *    main.js
 *    Mastering Data Visualization with D3.js
-*    Project 1 - Star Break Coffee
+*    Project 2 - Gapminder Clone
 */
 
 const m = {
-  t: 20,
-  l: 100,
-  r: 20,
-  b: 60
+	t: 20,
+	r: 20,
+	b: 100,
+	l: 100
 };
-const w = 600;
-const h = 400;
-const width = w - m.r - m.l;
-const height = h - m.t - m.b;
 
-const t = d3.transition().duration(750);
+const w = 600;
+const w2 = 600 - m.r - m.l;
+const h = 400;
+const h2 = h - m.t - m.b;
 
 let svg = d3.select("#chart-area")
-  .append("svg")
-  .attr("width", w)
-  .attr("height",h);
+	.append("svg")
+	.attr("height",h)
+	.attr("width",w)
 
 let g = svg.append("g")
-  .attr("transform","translate("+ m.l +","+ m.t +")");
+	.attr("transform","translate("+m.l+","+m.t+")")
 
-let xLabel = g.append("text")
-  .attr("x",width / 2)
-  .attr("y",height + 50)
-  .attr("text-anchor","middle")
-  .attr("font-size","20px")
-  .text("Month")
-
-let yLabel = g.append("text")
-  .attr("x",-(height / 2))
-  .attr("y",-60)
-  .attr("transform","rotate(-90)")
-  .attr("text-anchor","middle")
-  .attr("font-size","20px")
-  .text("Revenue")
-
-
-d3.json("./data/revenues.json").then(data =>{
-  
-  data.forEach( e => {
-    e.revenue = + e.revenue;
-    e.profit = + e.profit;
-  });
-  let x = d3.scaleBand()
-    .range([0,width])
-    .paddingInner(.3)
-    .paddingOuter(.3)
-
-  let y = d3.scaleLinear()
-    .range([height,0])
-
-  let xAxis = g.append("g")
-    .attr("class","top axis")
-    .attr("transform","translate(0,"+ height +")")
-
-  let yAxis = g.append("g")
-    .attr("class","top axis")
-    
-  let rects = g.selectAll("rect")
-
-  let flag = true;
-
-  d3.interval(()=>{
-    const newData = flag ? data : data.slice(1,3);
-    update(newData);
-    flag = !flag;
-  },1000)
-
-  update(data)
-
-  function update(data){
-
-    const which = flag ? "revenue" : "profit";
-
-    x.domain(data.map(e => e.month))
-    y.domain([0,d3.max(data,d=>d[which])])
-
-    xAxis
-      .transition(t)
-      .call(d3.axisBottom(x))
-        .selectAll("text")
-        .attr("text-anchor","middle")
-  
-    yAxis
-      .transition(t)
-      .call(
-        d3.axisLeft(y)
-        .tickFormat(d => "$" + d)
-      )
-
-    let rects = g.selectAll("rect")
-      .data(data,d => d.month)
-
-    rects.exit()
-      .attr("fill","red")
-      .transition(t)
-      .attr("y",y(0))
-      .attr("height",0)
-      .remove()
+d3.json("data/data.json").then(function(data){
+	
+	// データ整形
+	data.forEach(d => {
+		d.countries = d.countries.filter(c => {
+			let flg = Object.keys(c).filter(k => {
+				return c[k] === null;
+			});
+			return flg.length === 0;
+		})
+	});
 
 
 
-    rects.enter().append("rect")
-      .attr("x",d => x(d.month))
-      .attr("width",x.bandwidth)
-      .attr("fill","grey")
-      .attr("y",y(0))
-      .attr("height",0)
-      .merge(rects)
-      .transition(t)
-      .attr("x",d => x(d.month))
-      .attr("width",x.bandwidth)
-      .attr("y",d => y(d[which]))
-      .attr("height",d => (height - y(d[which])))
-    
+	let x = d3.scaleLog()
+		.domain([300,150000])
+		.range([0,w2])
+	let y = d3.scaleLinear()
+		.domain([0,90])
+		.range([h2,0])
+	let r = d3.scaleLinear()
+		.range([5,25])
 
-  // rects.update().append("rect")
-  //   .attr("x",d => x(d.month))
-  //   .attr("width",x.bandwidth)
-  //   .attr("y",d => y(d[which]))
-  //   .attr("height",d => (height - y(d[which])))
-  //   .attr("fill","grey")
-    
-    yLabel.text(which.charAt(0).toUpperCase() + which.slice(1))
 
-  }
+	function update(data){
+
+		r
+		.domain([
+			d3.min(data,d => d.population) / 5 * Math.PI,
+			d3.max(data,d => d.population) / 5 * Math.PI
+		])
+
+		let scat = g.selectAll("circle")
+			.data(data)
+
+		scat.exit().remove()
+
+		scat
+			.attr("cx",d => x(d.income))
+			.attr("cy",d => y(d.life_exp))
+			.attr("r",d => r(d.population))
+			.attr("fill","blue")
+
+		scat.enter().append("circle")
+			.attr("cx",d => x(d.income))
+			.attr("cy",d => y(d.life_exp))
+			.attr("r",d => r(d.population))
+			.attr("fill","blue")
+	}
+
+	let idx = 0;
+	let len = data.length;
+
+	d3.interval(()=>{
+
+		let newData = data[idx % len].countries;
+		let year = data[idx % len].year;
+		update(newData)
+		idx++;
+
+	},100)
 
 
 
 
-});
+
+	
+	
+})
